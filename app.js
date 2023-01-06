212c4d952341bd528894fa6dcec333948413e550
Lilynews <lilinews2021@gmail.com>
2023-01-05 21:56:37 -0500

    feat: create template engin & partial template

--- app.js
+++ app.js
@@ -3,11 +3,26 @@ const express = require('express')
 const app = express()
 const port = 3000
 const host = `http://localhost:${port}`
+const exphbs = require('express-handlebars')
+require('mongoose')
+
+// load JSON file
+const moviesJson = require('./models/seeds/movies.json')
+
+// setting express-handlebars
+app.engine('hbs', exphbs({defaultLayout: 'main', extname:'.hbs'}))
+app.set('view engine','hbs')
+
+
+
+// "use" in express
+app.use(express.static('public'))
 
 
 app.get('/',(req,res) => {
-  res.send('yoooooooo')
+  res.render('index')
 })
+
 app.listen(port, () => {
   console.log(`${projectName} app is running on ${host}`)
 })

97efbff4d91d22d2d9ce61e31d4a650d5f317697
Lilynews <lilinews2021@gmail.com>
2023-01-09 10:22:02 -0500

    feat: edit comment

--- app.js
+++ app.js
@@ -1,75 +1,97 @@
 //////// load 3-rd-party-tools, variable
-const projectName = 'movie-list-010323'
-const express = require('express')
+const projectName = "movie-list-010323"
+const express = require("express")
 const app = express()
 const port = 3000
 const host = `http://localhost:${port}`
-const exphbs = require('express-handlebars')
-require('mongoose')
-const moviesDB = require('./models/movieSchema')
-const movieImgAPI = 'https://movie-list.alphacamp.io/posters/'
+const exphbs = require("express-handlebars")
+require("./config/mongoose")
+const moviesDB = require("./models/movieSchema")
 
-//////// load JSON file
-const moviesJson = require('./models/seeds/movies.json').results
 
 //////// setting express-handlebars
-app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
-app.set('view engine', 'hbs')
-
-
+app.engine("hbs", exphbs({
+  defaultLayout: "main",
+  extname: ".hbs",
+  helpers: {
+    selected: (sortSelected) => {
+      if (sortSelected) return "selected"
+    }
+  }
+}))
+app.set("view engine", "hbs")
 
 //////// "use" in express
-app.use(express.static('public'))
+app.use(express.static("public"))
 
 //////// Router
-// index-router (DB)
-// app.get('/', (req, res) => {
-//   moviesDB.find()
-//     .lean()
-//     .then(movieData => {
-//       return res.render('index', { title: 'Movie List', movieData, movieImgAPI })
-//     })
-//     .catch(error => console.log('get router error'))
-// })
 
-// index-router(JSON)
+
+//SearchBar
 app.get('/', (req, res) => {
-  res.render('index', { title: 'Movie List', movieData: moviesJson, movieImgAPI })
-  console.log(movieImgAPI)
+  const keyword = (req.query.keyword) ? req.query.keyword.trim() : ''
+  const sort = req.query.sort || 'default'
+  const sortBy = {
+    default: { id: 'asc' },
+    AtoZ: { title: 'asc' },
+    ZtoA: { title: 'desc' },
+  }
+
+  const sortSelected = { [sort]: true }
+  moviesDB.find()
+    .lean()
+    .sort(sortBy[sort])
+    .then(
+      movies => {
+        const movieResult = movies.filter(movie => {
+          return movie.title.toLowerCase().includes(keyword.toLowerCase())
+        })
+
+        if (!keyword) {
+          res.render('index', {
+            title: 'Movie List',
+            movies,
+            sortSelected
+          })
+        } else {
+          res.render('index', {
+            title: 'Movie List',
+            movies: movieResult,
+            keyword,
+            sortSelected
+          })
+        }
+      })
+    .catch(error => {
+      console.log(error)
+      // console.log('search router error')
+    })
 })
 
-// index-router(EASY)
-// app.get('/', (req, res) => {
-//   res.render('index', { title: 'Movie List', movieImgAPI })
-
-//   console.log(moviesDB
-//     .find()
-//     .lean()
-//     .then(
-//       movieData => movieData.title
-//       // Promise { <pending> }?
-//       ))
-// })
-
-// detail-dynamic-router(JSON)
-app.get('/movies/:movie_id', (req, res) => {
-  const movie = moviesJson.find(movie => {
-    return movie.id.toString() === req.params.movie_id
-  })
-  res.render('detail', { movie })
-})
 // detail-dynamic-router(DB)
-// app.get('/movies/:movie_id', (req, res) => {
-//   const id = req.params.movie_id
-//   return moviesDB.findById(id)
-//     .lean()
-//     .then ( movie => {
-//       return res.render('detail', { movie })
-//     })
-//     .catch(error => console.log('dynamic router error'))
-// })
+app.get("/movies/:movie_id", (req, res) => {
+  const id = req.params.movie_id
+  console.log(id)
+  // is findOne not findById
+  return (
+    moviesDB
+      .findOne({ id })
+      // .exec()
+      .lean()
+      .then((movie) => {
+        return res.render("detail", {
+          title: 'Movie Detail',
+          movie
+        })
+      })
+      .catch(error => {
+        console.log(error)
+        console.log("dynamic router error")
+      })
+  )
+})
 
-//SearchBar (JSON)
+
 
 
 //////// Listener
